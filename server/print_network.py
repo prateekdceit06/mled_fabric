829686c02b79b4a4275364d09119fe283d664891
prateek <jainp@bu.edu>
2023-08-08 00:49:56 -0400

    Printing the MLED structure done.

--- server/print_network.py
+++ server/print_network.py
@@ -1,22 +1,27 @@
 from print_colour import PrintColor
 import json
 
+
 def read_master_config(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
+
 def center_string(s):
     return s.center(17, ' ')
+
 
 class PrintNetwork:
 
     def __init__(self, file_path):
         self.file_path = file_path
         self.data = read_master_config(self.file_path)
-        self.last_layer_length = len(self.data['layers'][0]['nodes'])
+        self.arrange_nodes()
+        self.layer_wise_nodes = [layer['nodes'] for layer in self.data['layers']]
+        self.last_layer_length = len(self.data['layers'][-1]['nodes'])
         self.printing_length = 2 * self.last_layer_length - 1
-        self.layer_wise_nodes = [layer['nodes'] for layer in self.data['layers']][::-1]
+
 
     def _print_line(self, nodes_in_layer, connection_block_number, content_func, last_layer=False):
         for index, node in enumerate(nodes_in_layer):
@@ -36,12 +41,23 @@ class PrintNetwork:
 
             last_layer = out_index == len(self.layer_wise_nodes) - 1
 
-            self._print_line(nodes_in_layer, connection_block_number, lambda node: PrintColor.print_in_green_back(center_string(node["name"])), last_layer)
-            self._print_line(nodes_in_layer, connection_block_number, lambda node: PrintColor.print_in_green_back(center_string(node["ip"])))
+            self._print_line(nodes_in_layer, connection_block_number,
+                             lambda node: PrintColor.print_in_green_back(center_string(node["name"])), last_layer)
+            self._print_line(nodes_in_layer, connection_block_number,
+                             lambda node: PrintColor.print_in_green_back(center_string(node["ip"])))
 
             if not last_layer:
-                self._print_line(nodes_in_layer, connection_block_number, lambda node: PrintColor.print_in_red(center_string("\u2191")))
-                self._print_line(nodes_in_layer, connection_block_number, lambda node: PrintColor.print_in_red(center_string("|")))
-                self._print_line(nodes_in_layer, connection_block_number, lambda node: PrintColor.print_in_red(center_string("\u2193")))
+                self._print_line(nodes_in_layer, connection_block_number,
+                                 lambda node: PrintColor.print_in_red(center_string("\u2191")))
+                self._print_line(nodes_in_layer, connection_block_number,
+                                 lambda node: PrintColor.print_in_red(center_string("|")))
+                self._print_line(nodes_in_layer, connection_block_number,
+                                 lambda node: PrintColor.print_in_red(center_string("\u2193")))
 
         print(PrintColor.print_in_green(PrintColor.divider()))
+
+    def arrange_nodes(self):
+        self.data['layers'] = sorted(self.data['layers'], key=lambda layer: len(layer['nodes']))
+        for layer in self.data['layers']:
+            layer['nodes'] = sorted(layer['nodes'], key=lambda node: node['node_id'])
+
