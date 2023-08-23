9dee2a6fc74472e2347907559bf26bae3eceb4a8
prateek <jainp@bu.edu>
2023-08-23 16:19:50 -0400

    master_config and ip_list sending updated

--- 3-5/ip_list_config.py
+++ 3-5/ip_list_config.py
@@ -5,7 +5,12 @@ class IPListConfigHandler:
 
     def get_ip_list_config(self, client_socket):
         length = int.from_bytes(client_socket.recv(4), byteorder='big')
-        data = client_socket.recv(length)
+        data = bytearray()
+        while len(data) < length:
+            packet = client_socket.recv(length - len(data))
+            if not packet:
+                raise Exception("Socket connection broken")
+            data.extend(packet)
         with open(self.ip_list_config_file_path, 'wb') as f:
             f.write(data)
 
